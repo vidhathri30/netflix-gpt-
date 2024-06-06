0244c4db36607f275a25244048edd1ce0e50c761
vidhathri30 <vidhathri30@gmail.com>
2024-06-07 00:25:05 +0530

    m

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@
 import './App.css';
 
 function App(){
-  return(<div className='text-3xl font-bold text-pink-700'>Hello lets create netflix gpt </div>)
+  return(<div className='text-3xl font-bold text-pink-700'>Hello lets create netflix gpt ....</div>)
 }
 export default App;
